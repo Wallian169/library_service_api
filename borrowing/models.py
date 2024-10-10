659822df4e8bf60816a3406a403ee708d584e834
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-10-10 13:18:19 +0300

    created Borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+        db_column="book_id",
+    )
+    user_id = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+        db_column="user_id",
+    )
