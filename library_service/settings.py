f1a6f48ece86fbf5ffd69abbf21310bb256820da
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-10-13 17:41:56 +0300

    removed sensitive data from repo

--- library_service/settings.py
+++ library_service/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6#0kt)hho#4r6#70y+sh$v$q@eyd*meh&&wl93#23ieij$e&r7"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
